25d3b025d94f417514396284037d4c3a75911571
geronde <mail>
2020-07-29 03:15:58 +0100

    Updates

---
+++ precache-manifest.9c3a3d9f6fb376ee924554e7ad15b9f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e580a257f3e180d77a917156e739a7",
+    "url": "/geronde/pokeapi-challenge/index.html"
+  },
+  {
+    "revision": "72a85e557cf0e31dab87",
+    "url": "/geronde/pokeapi-challenge/static/js/2.cb7290c9.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/geronde/pokeapi-challenge/static/js/2.cb7290c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9380afb6f30167be3c1",
+    "url": "/geronde/pokeapi-challenge/static/js/main.b2091bd0.chunk.js"
+  },
+  {
+    "revision": "b652c5fe810d6c32b89f",
+    "url": "/geronde/pokeapi-challenge/static/js/runtime-main.76c9711e.js"
+  }
+]);
