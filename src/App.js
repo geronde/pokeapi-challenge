7684edfffb62c54c1d9f6c8013676df574636194
geronde <mail>
2020-07-29 03:35:11 +0100

    add fix

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './containers/HomePage'
 import Pokemon from './containers/Pokemon';
 import Types from './containers/Types';
+import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div>
-     <BrowserRouter>
+     <HashRouter>
      <Switch>
        <Route path="/"  component={HomePage} exact/>
        <Route path="/pokemon/:id"  component={Pokemon}/>
        <Route path="/types/:type"  component={Types}/>
      </Switch>
-     </BrowserRouter>
+     </HashRouter>
     </div>
   );
 }
