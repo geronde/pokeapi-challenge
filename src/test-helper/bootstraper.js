91f2c8b15c1b45d897f511e987cee5d6113dea18
geronde <geronde@docker.home>
2020-11-02 22:05:27 +0100

    test bootstrap

---
+++ src/test-helper/bootstraper.js
@@ -0,0 +1,14 @@
+import faker from 'faker';
+
+export const generateTestPokemons = (n = 15, isType) => {
+    const pokemons = [];
+
+    for(let i = 0; i < n; i++) {
+        let poke = { name: faker.random.word(), url: `fake-url/${i}/` }
+        if(isType){
+            poke = {pokemon: {...poke}}
+        }
+        pokemons.push(poke)
+    }
+    return pokemons
+}
